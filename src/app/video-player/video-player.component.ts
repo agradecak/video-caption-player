04a7e6f76a788cee03f8a2ce264b3dd28760dbee
Andrija Gradečak <andrija.gradecak@gmail.com>
2025-05-29 18:06:35 +0200

    Add logic for initial video loading and switching via buttons.

--- src/app/video-player/video-player.component.ts
+++ src/app/video-player/video-player.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-video-player',
@@ -7,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrl: './video-player.component.scss'
 })
 export class VideoPlayerComponent {
+  @ViewChild('videoElement', { static: true }) videoElement!: ElementRef<HTMLVideoElement>;
+
+  currentVideo: string = 'video_1';
+  videoOptions = [
+    { value: 'video_1', label: 'Clip 1' },
+    { value: 'video_2', label: 'Clip 2' }
+  ]
+
+  ngOnInit() {
+    this.loadVideo();
+  }
+
+  async loadVideo() {
+    const video = this.videoElement.nativeElement;
+    video.src = `${this.currentVideo}/clip.mp4`;
+  }
+
+  switchVideo(videoName: string) {
+    this.currentVideo = videoName;
+    this.loadVideo();
+  }
+
 
 }
